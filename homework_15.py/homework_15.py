4f5e31c614ad3e6679109f6ef23ec0c2cd9b28e5
annamariaqa <polovykhanna@gmail.com>
2024-11-16 16:52:43 +0200

    Update for file homework 15

--- homework_15.py/homework_15.py
+++ homework_15.py/homework_15.py
@@ -10,16 +10,17 @@ class Rhombus:
                 print('side a value must be bigger than 0')
                 value = 0
             super().__setattr__(name, value)
-        elif name == 'angle_a':
+        if name == 'angle_a':
             if hasattr(self, 'angle_b') and not (value == 180 - self.angle_b):
                 print('angle b plus angle a must be 180')
-                value = 180 - self.angle_b
-            super().__setattr__(name, value)
-        elif name == 'angle_b':
+            super().__setattr__('angle_a', value)
+            super().__setattr__('angle_b', 180 - self.angle_a)
+        if name == 'angle_b':
             if hasattr(self, 'angle_a') and not (value == 180 - self.angle_a):
                 print('angle b plus angle a must be 180')
-                value = 180 - self.angle_a
             super().__setattr__(name, value)
-
+            super().__setattr__('angle_a', 180 - self.angle_b)
 
 rhombus_instance = Rhombus(side_a=10, angle_b=90, angle_a=90)
+rhombus_instance.angle_b = 70
+print(rhombus_instance.angle_a, rhombus_instance.angle_b)
