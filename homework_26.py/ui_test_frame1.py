cd9f4e92e49eca6ec6fac8ae0fe1463499ed9c2b
annamariaqa <polovykhanna@gmail.com>
2024-12-31 13:05:43 +0200

    Files for homework 26

---
+++ homework_26.py/ui_test_frame1.py
@@ -0,0 +1,39 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import ChromiumOptions
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.alert import Alert
+import time
+
+
+chrome_options = ChromiumOptions()
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(options=chrome_options, service=service)
+
+driver.get("http://localhost:8000/dz.html")
+
+
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(1)  
+
+driver.execute_script("window.scrollTo(0, 0);")
+time.sleep(1)  
+
+driver.switch_to.frame(driver.find_element(By.ID, "frame1"))
+
+
+for _ in range(1):
+    input = driver.find_element(By.ID, "input1")
+    input.send_keys("Frame1_Secret")
+    frame1_button = driver.find_element(By.XPATH, "//button[@onclick=\"verifyInput('input1')\"]")
+    frame1_button.click()
+    alert = Alert(driver)
+    print("Верифікація пройшла успішно!", alert.text)
+    alert.accept()
+    time.sleep(1)  
+
+# Закриваємо браузер
+driver.quit()
